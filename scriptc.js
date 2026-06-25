17198307ca6c432a69762d5d1e7b88ec8b6ff9e7
RamNarenSurya <suryaram238@gmail.com>
2026-06-25 18:37:45 +0530

    Update and rename scriptb.js to scriptc.js

---
+++ scriptc.js
@@ -0,0 +1,7 @@
+let textEl = document.getElementById("text");
+let buttonTextEl=document.getElementById("buttonText");
+
+buttonTextEl.addEventListener("click",function(){
+    textEl.textContent="WelCome Surya...!";
+    textEl.style.color="red";
+})
